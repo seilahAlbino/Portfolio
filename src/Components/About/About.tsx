301b9aef2d1d3dd916b01c86831d2b4d5b5681b5
seilahAlbino <pereirajoaquim312@gmail.com>
2024-10-10 23:24:56 +0100

    Changed text about me

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -5,24 +5,31 @@ import TechnologyCard from "../Cards/TechnologyCard";
 function About() {
     return (
         <section className="p-8 px-12">
-            <h2 className="text-4xl font-bold mb-12 text-center text-primary aboutTitle">Sobre</h2>
+            <h2 className="text-4xl font-bold mb-12 text-center text-primary aboutTitle">
+                Sobre
+            </h2>
             <div className="flex flex-col lg:flex-row lg:justify-between gap-4">
                 <div className="lg:w-1/2 mb-6 lg:mb-0 aboutText">
-                    <h3 className="text-2xl font-semibold mb-4">Mais sobre mim</h3>
-                    <p className="text-md md:text-lg">
-                        Atualmente, estou a estudar Engenharia Informática blablablablablab<br/>lablablablabla<br/>blablablablablabla<br/>blablablabla<br/>blablablablablablablabla
+                    <h3 className="text-2xl font-semibold mb-4">
+                        Mais sobre mim
+                    </h3>
+                    <p className="text-md md:text-lg ">
+                    Sou um estudante de Engenharia Informática com uma paixão por tecnologia. 
+                    <br/>
+                    Estou sempre em busca de novos desafios que me permitam aplicar as minhas habilidades em desenvolvimento web e criar experiências interativas.
                     </p>
                 </div>
                 <div className="lg:w-1/2 tecnologias">
                     <h3 className="text-2xl font-semibold mb-4">Tecnologias</h3>
                     <ul className="flex flex-wrap gap-4 list-none">
-                        {Technologies.map(tecnology =>
+                        {Technologies.map((tecnology) => (
                             <li key={tecnology.name}>
                                 <TechnologyCard
                                     name={tecnology.name}
-                                    icon={tecnology.icon} />
+                                    icon={tecnology.icon}
+                                />
                             </li>
-                        )}
+                        ))}
                     </ul>
                 </div>
             </div>
